87cccbad018a2712ac48b8cf24b71fc747a230da
aliatayee <aliaqaatayee@gmail.com>
2022-08-10 16:59:47 +0430

    Add multible tables with data

--- data.sql
+++ data.sql
@@ -13,3 +13,24 @@ VALUES
 (null,'Boarmon', '2005-06-07', 7, true, '20.4'),
 (null,'Blossom', '1998-10-13', 3, true, '17'),
 (null,'Ditto', '2022-05-14', 4, true, '22');
+
+INSERT INTO owners (id, full_name,age)
+VALUES 
+(null,'Sam Smith', '34'),
+(null,'Jennifer Orwell', '19'),
+(null,'Bob', '45'),
+(null,'Melody Pond ', '77'),
+(null,'Dean Winchester ', '14'),
+(null,'Jodie Whittaker', '38');
+INSERT INTO species (id, name)
+VALUES 
+(null,'Pokemon'),
+(null,'Digimon');
+
+UPDATE animals SET species_id = '2' WHERE name LIKE '%mon%';
+UPDATE animals SET species_id = '1' WHERE species_id is null;
+UPDATE animals SET owner_id = '1' WHERE name LIKE 'Agumon';
+UPDATE animals SET owner_id = '2' WHERE name LIKE 'Gabumon' OR name LIKE 'Pikachu';
+UPDATE animals SET owner_id = '3' WHERE name LIKE 'Devimon' OR name LIKE 'Plantmon';
+UPDATE animals SET owner_id = '4' WHERE name LIKE 'Charmander' OR name LIKE 'Squirtle' OR name LIKE 'Blossom';
+UPDATE animals SET owner_id = '5' WHERE name LIKE 'Angemon' OR name LIKE 'Boarmon';
--- queries.sql
+++ queries.sql
@@ -47,3 +47,9 @@ SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+--What animals belong to Melody Pond?
+SELECT  * FROM animals
+   INNER JOIN owners
+   ON owners.id = animals.owner_id
+   WHERE full_name LIKE '%Melody Pond%';
