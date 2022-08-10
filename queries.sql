a03554e603d08971c8c0dfc591b665de334656ba
aliatayee <aliaqaatayee@gmail.com>
2022-08-10 14:37:01 +0430

    Add column & insert new data

--- queries.sql
+++ queries.sql
@@ -8,3 +8,7 @@ SELECT name, escape_attempts FROM `animals` WHERE (weight_kg> 10.5);
 SELECT * FROM `animals` WHERE neutered = true;
 SELECT * FROM `animals` WHERE NOT name = 'Gabumon';
 SELECT * FROM `animals` WHERE weight_kg BETWEEN 10.4  AND 17.3
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
