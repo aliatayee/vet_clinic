3b1e033957813170c5e81a52eb3a10b18b459793
aliatayee <aliaqaatayee@gmail.com>
2022-08-12 22:16:17 +0430

    Add join table data & queries

--- data.sql
+++ data.sql
@@ -34,3 +34,38 @@ UPDATE animals SET owner_id = '2' WHERE name LIKE 'Gabumon' OR name LIKE 'Pikach
 UPDATE animals SET owner_id = '3' WHERE name LIKE 'Devimon' OR name LIKE 'Plantmon';
 UPDATE animals SET owner_id = '4' WHERE name LIKE 'Charmander' OR name LIKE 'Squirtle' OR name LIKE 'Blossom';
 UPDATE animals SET owner_id = '5' WHERE name LIKE 'Angemon' OR name LIKE 'Boarmon';
+
+INSERT INTO vets (id, name,age,date_of_graduation)
+VALUES 
+(null,'Vet William Tatcher',  45,  '2000-04-23'),
+(null,'Vet Maisy Smith',  26, '2019-01-17'),
+(null,'Vet Stephanie Mendez',  64, '1981-05-04'),
+(null,'Vet Jack Harkness',  38,  '2008-06-08');
+INSERT INTO specializations (vets_id, species_id)
+VALUES 
+(1,1),
+(3,1),
+(3,2),
+(4,2);
+INSERT INTO visits (vets_id, animal_id,visit_date)
+VALUES 
+(1,1,'2020-05-24'),
+(3,1,'2020-07-22'),
+(4,2,'2021-02-02'),
+(2,3,'2020-01-05'),
+(2,3,'2020-03-08'),
+(2,3,'2020-05-14'),
+(3,4,'2021-05-04'),
+(4,5,'2021-02-24'),
+(2,6,'2019-12-21'),
+(1,6,'2020-08-10'),
+(2,6,'2021-04-07'),
+(3,7,'2019-09-29'),
+(1,4,'2020-10-03'),
+(1,4,'2019-11-04'),
+(2,9,'2019-01-24'),
+(2,9,'2019-05-15'),
+(2,9,'2020-02-27'),
+(2,9,'2020-08-03'),
+(3,10,'2020-05-24'),
+(1,10,'2021-01-11');
